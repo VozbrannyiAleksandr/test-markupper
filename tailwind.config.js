d0fc21bbbfb14a7aec5e44c0559cdef230008462
VozbrannyiAleksandr <saniok2525@gmail.com>
2021-11-01 03:32:42 +0000

    finished desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'xs': '375px',
+      // => @media (min-width: 375px) { ... }
+    },
     fontFamily: {
       body: ['Montserrat']
      }
